742917995299aa9d3d86b0e8e817981c908aff8a
Gabriel Paes Leme <gabrielnascimentopaesleme@gmail.com>
2024-12-03 10:25:45 -0300

    Criando a tipagem de cada produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { ProductType } from "@/types/productType";
+
+const getProducts = async () => {
+  const response = await fetch('https://fakestoreapi.com/products');
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return response.json();
+};
+
+
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products)
   return (
-    <div>
-      <h1>E-commerce</h1>
+    <div className="container pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {products.map((product: ProductType, index: number) => (
+          <div key={index}>
+            <img src={product.image} alt={product.description} />
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <h3>{product.price}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/types/ProductType.ts
@@ -0,0 +1,8 @@
+export type ProductType = {
+  id: number;
+  price: number | null;
+  title: string;
+  description: string ;
+  image: string
+  category?: string
+};

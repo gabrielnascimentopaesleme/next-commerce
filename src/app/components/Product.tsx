641ca884252536cb008ad2fe911c21fe5e58cb19
Gabriel Paes Leme <gabrielnascimentopaesleme@gmail.com>
2024-12-03 11:42:45 -0300

    Estilizando o componente do produto com o componente de imagem

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,16 +1,25 @@
-import { ProductType } from '@/types/productType';
-import React from 'react';
-
+import { ProductType } from '@/types/ProductType';
+import ProductImg from './ProductImg';
 type ProductProps = {
   product: ProductType;
 };
 
 const Product = ({ product }: ProductProps) => {
   return (
-    <div className='flex flex-col shadow-lg h-96 rounded-md bg-white p-5 text-gray-600' key={product.id}>
-      <div className='relative max-h-72 flex-1'>img</div>
-      <div className='flex justify-between font-bold my-3'><h1>{product.title}</h1></div>
-      <button className='rounded-md bg-teal-300 text-white px-3.5 py-2.5 text-sm text-center'>Adicionar ao carrinho</button>
+    <div
+      className="flex flex-col shadow-lg h-96 rounded-md bg-white p-5 text-gray-600"
+      key={product.id}
+    >
+      <div className="relative max-h-72 flex-1">
+        <ProductImg product={product} fill />
+      </div>
+      <div className="flex justify-between font-bold my-3">
+        <p className='w-40 truncate'>{product.title}</p>
+        <p className='text-md text-green-400'>{product.price}</p>
+      </div>
+      <button className="rounded-md bg-teal-300 text-white px-3.5 py-2.5 text-sm text-center">
+        Adicionar ao carrinho
+      </button>
     </div>
   );
 };
---
+++ src/app/components/ProductImg.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { ProductType } from '@/types/ProductType';
+import Image from 'next/image';
+import { useState } from 'react';
+
+type ProductImgProps = {
+  product: ProductType;
+  fill?: boolean;
+};
+
+const ProductImg = ({ product, fill }: ProductImgProps) => {
+  const [loading, setLoading] = useState(true)
+  return fill ?(<Image src={product.image} alt={product.title} fill className={`object-cover ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`} onLoadingComplete={() => setLoading(false)}/>) : (<Image src={product.image} alt={product.title} width={400} height={700} className={`object-cover ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`} onLoadingComplete={() => setLoading(false)}/>)
+};
+
+export default ProductImg;
